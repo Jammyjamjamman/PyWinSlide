49d75ac721c05c4280fe21ff68d21c45c2318e7b
Jammyjamjamman <jdsherratt3@gmail.com>
2018-08-06 00:40:21 +0100

    Significantly improved the code, now it's object-oriented and caculates the mean and variance.

--- pywinslide.py
+++ pywinslide.py
@@ -3,14 +3,85 @@
 """
 Created on Wed Aug  1 23:12:47 2018
 
-@author: james
+@author: James Sherratt
 """
 from datetime import timedelta
 
 
-def moving_average(dat_iter, window_sz = timedelta(days=1)):
+class Ts_val_valsq(object):
     """
-    Creates an iterator which calculates the moving average
+    A class defining a "timeseries" object which contains a
+    timestamp, value and the square of that value.
+    
+    The constructor requires a timestamp and value.
+    """
+    def __init__(self, time, val):
+        self.time = time
+        self.val = val
+        self.val_sq = val*val
+    
+
+class Win_mean_meansq(object):
+    def __init__(self, init_Ts_val_valsq, win_sz):
+        self.win = [init_Ts_val_valsq]
+        self.sum = init_Ts_val_valsq.val
+        self.sum_sq = init_Ts_val_valsq.val_sq
+        self.win_len = 1
+        self.half_win_sz = win_sz/2
+        self.next_element_update()
+    
+    
+    def append_dat(self, ts_val_valsq_ins):
+        self.win.append(ts_val_valsq_ins)
+        self.sum += ts_val_valsq_ins.val
+        self.sum_sq += ts_val_valsq_ins.val_sq
+        self.win_len += 1
+        
+    
+    def remove_dat(self):
+        while self.win[0].time < self.cur_win_min:
+            last_ts_val_valsq_ins = self.win.pop(0)
+            self.sum -= last_ts_val_valsq_ins.val
+            self.sum_sq -= last_ts_val_valsq_ins.val_sq
+            self.win_len -= 1
+            self.cur_win_pos -= 1
+    
+    
+    def update_with_other_ts_val(self, ts_val_valsq_ins):
+        self.cur_win_time = ts_val_valsq_ins.time
+        self.cur_win_min = self.cur_win_time - self.half_win_sz
+        self.cur_win_max = self.cur_win_time + self.half_win_sz
+    
+    
+    def next_element_update(self):
+        try:
+            self.cur_win_pos += 1
+        except AttributeError:
+            self.cur_win_pos = 0
+        self.cur_win_time = self.win[self.cur_win_pos].time
+        self.cur_win_min = self.cur_win_time - self.half_win_sz
+        self.cur_win_max = self.cur_win_time + self.half_win_sz
+        self.remove_dat()
+        
+        
+    
+    def get_stats(self):
+        try:
+            return (self.cur_win_time,
+                    self.sum/self.win_len,
+                    (self.sum**2 - self.sum_sq)/(self.win_len - 1))
+        except ZeroDivisionError:
+            return (self.cur_win_time,
+                    self.sum/self.win_len,
+                    None)
+
+
+def sliding_mean_var(dat_iter,
+                     window_sz = timedelta(days=1),
+                     Ts_cls = Ts_val_valsq,
+                     Win_cls = Win_mean_meansq):
+    """
+    Creates an iterator which calculates the rolling mean and variance
     from a timeseries iterator.
     The time series iteratore should return values in ascending order of time.
     
@@ -21,8 +92,8 @@ def moving_average(dat_iter, window_sz = timedelta(days=1)):
         Default window size is 1 day.
     
     Yields:
-        (datetime, float): The next "moving average" as a tuple containing the
-        timestamp and datetime.
+        (datetime, float, float): The next "rolling mean and variance"
+        as a tuple containing the timestamp and datetime.
         
     TODO:
     1. Improve commments.
@@ -30,75 +101,27 @@ def moving_average(dat_iter, window_sz = timedelta(days=1)):
     3. Add tests.
     4. Write a more cython-friendly version for speed improvements
     """
-    half_window = window_sz/2
-
-    window = []
-    cur_win_pos = 0
-
-    next_time, next_val = next(dat_iter)
-
-    win_min = next_time - half_window
-    win_max = next_time + half_window
-
-    # Calculate sum of elements in the window.
-    # This sum will be modified as values are
-    # appended and removed from the window.
-    while next_time <= win_max:
-        window.append((next_time, next_val))
-        try:
-            next_time, next_val = next(dat_iter)
-        except StopIteration:
-            break
-
-    # Calculate sum of elements in the window.
-    # This sum will be modified as are appended
-    # and removed from the window.
-    cur_sum = sum(dat[1] for dat in window)
-    # keep a running total of the elements in the window.
-    # This will change as elements are added/ removed from the window
-    window_len = len(window)
-
-    # 2. Calculate the means.
-    # The loop is finished when the last element in the window is reached.
-    while cur_win_pos != window_len:
-        win_min = window[cur_win_pos][0] - half_window
-        win_max = window[cur_win_pos][0] + half_window
-
-        # 2.1 Remove values not within current window bounds.
-        while window[0][0]  < win_min:
-            cur_sum -= window.pop(0)[1]
-            cur_win_pos -= 1
-            window_len -= 1
-
-        # 2.2 Insert the next values from iterator, within window.
-        while next_time <= win_max:
-            try:
-                temp_time, temp_val = next_time, next_val
-                next_time, next_val = next(dat_iter)
-            except StopIteration:
-                break
-            else:
-                window.append((temp_time, temp_val))
-                window_len += 1
-                cur_sum += temp_val
+    
+    next_dat = Ts_cls(*next(dat_iter))
+    
+    window = Win_cls(next_dat, window_sz)
+    
+    for dat in dat_iter:
+        next_dat = Ts_cls(*dat)
         
-        # 2.3 Complete and emit the mean calculation.
-        yield window[cur_win_pos][0], (cur_sum/window_len)
-        cur_win_pos += 1
-
-        # 3. If we have reached the end of the window,
-        # check if there's any values left in the iterator.
-        if cur_win_pos == window_len:
+        while next_dat.time > window.cur_win_max:
+            window.remove_dat()
+            
+            yield window.get_stats()
+            
             try:
-                temp_time, temp_val = next_time, next_val
-                next_time, next_val = next(dat_iter)
-            except StopIteration:
-                pass
-            else:
-                window.append((temp_time, temp_val))
-                cur_sum += temp_val
-                window_len += 1
-    
-    
+                window.next_element_update()
+            except IndexError:
+                window.update_with_other_ts_val(next_dat)
+                break
+        
+        window.append_dat(next_dat)
+
+
 # TODO: write other slidng window functions
 # e.g. rolling variance, custom rolling functions
